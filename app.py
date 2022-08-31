5f5ac86c74b87c85cff84589b4fbefae3e406978
fairlabs_1 <fairlabs@fairlabs-1ui-MacBookPro.local>
2022-08-31 14:37:28 +0900

    add dropdown inside home.html

--- app.py
+++ app.py
@@ -3,16 +3,8 @@ app = Flask(__name__)
 app.debug = True
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return render_template('home.html')
-
-@app.route('/')
 def home():  # put application's code here
-    return render_template('dropdown.html')
-
-if __name__ == "__main__":
-    app.run()
-
+    return render_template('home.html')
 
 @app.route('/about')
 def about():  # put application's code here
