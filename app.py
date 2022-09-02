911ac8861376d8952bc37566aacecb7bafae20bc
ulan <argymak@unist.ac.kr>
2022-09-02 15:30:21 +0900

    All the input were included as a class. However, displaying is a mess...

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request
 from service import CostEstimationService
+from models import Schema
+
 app = Flask(__name__)
 app.debug = True
 
 @app.route('/')
-def home():  # put application's code here
+def home():
     return render_template('home.html')
 
 @app.route('/cost', methods =["GET", "POST"])
@@ -12,13 +14,13 @@ def cost():
     if request.method == "POST":
        # getting input with name = fname in HTML form
         customer = request.form.get("customer")
-        layer_thicknes = request.form.get("customer")
+        layer_thickness = request.form.get("customer")
         CostEstimationService().create({'customer': customer, })
 
     return render_template("home.html")
 
 @app.route('/about')
-def about():  # put application's code here
+def about():
     return render_template('about.html', title='about')
 
 if __name__ == '__main__':
