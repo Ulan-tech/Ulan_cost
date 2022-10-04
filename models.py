a9a343eb4d00119db096c33f8cfa3601488ab0b9
ulan <argymak@unist.ac.kr>
2022-10-04 22:18:01 +0900

    last changes

--- models.py
+++ models.py
@@ -9,45 +9,25 @@ class Schema:
         # what happens if we swap them?
 
     def create_part_information_table(self):
-        part_query = """
+        query = """
         CREATE TABLE IF NOT EXISTS "part_information" (
             customer text,
-            part_id INTEGER,
-            material_type text,
-            number_of_parts NUMERIC,
-            part_volume NUMERIC,
-            support_volume NUMERIC,
-            scan_speed NUMERIC, 
-            surface_area NUMERIC,
-            box_volume NUMERIC,
-            material_cost NUMERIC,
-            build_id INTEGER
-        );
-        """
-
-        build_query = """
-        CREATE TABLE IF NOT EXISTS "build_information" (
-            build_id INTEGER,
             material_type text,
             hatch_distance NUMERIC,
             num_of_layers NUMERIC,
+            scan_speed NUMERIC, 
             layer_thickness NUMERIC,
             build_time NUMERIC,
+            number_of_parts NUMERIC,
+            part_volume NUMERIC,
+            support_volume NUMERIC,
+            surface_area NUMERIC,
+            box_volume NUMERIC,
             max_build_height NUMERIC,
             wire_cut text,
             heat_treat text,
-            build_cost NUMERIC
+            total_cost NUMERIC
+            
         );
         """
-
-        cost_query = """
-        CREATE TABLE IF NOT EXISTS "cost_information" (
-            part_id INTEGER,
-            part_cost NUMERIC,
-            total_cost NUMERIC      
-        );
-        """
-
-        self.conn.execute(part_query)
-        self.conn.execute(build_query)
-        self.conn.execute(cost_query)
+        self.conn.execute(query)
--- service.py
+++ service.py
@@ -1,117 +1,57 @@
 import sqlite3
-import uuid
 
-class DataInjection:
+class PartInfoModel:
+    TABLENAME = "part_information"
+
     def __init__(self):
-        self.conn = sqlite3.connect('slm.db', timeout=10)
+        self.conn = sqlite3.connect('slm.db')
 
     def insert(self, customer, material_type, hatch_distance, num_of_layers, scan_speed, layer_thickness, build_time, number_of_parts,part_volume,support_volume,
-               surface_area, box_volume, max_build_height, wire_cut, heat_treat, build_cost, mat_cost, build_id):
-        
-        part_id = 1
-        cur = self.conn.cursor()
-        part_query = f"""insert into "part_information" """\
+               surface_area, box_volume, max_build_height, wire_cut, heat_treat, total_cost):
+        query = f"""insert into {self.TABLENAME} """\
                 f"""(
                         customer,
-                        part_id,
-                        material_type,
-                        number_of_parts,
-                        part_volume,
-                        support_volume,
-                        scan_speed, 
-                        surface_area,
-                        box_volume,
-                        material_cost,
-                        build_id
-                    )""" \
-                f"""values ('{customer}',
-                            {part_id},
-                            '{material_type}',
-                            {number_of_parts},
-                            {part_volume},
-                            {support_volume}, 
-                            {scan_speed},
-                            {surface_area}, 
-                            {box_volume}, 
-                            {mat_cost},
-                            {build_id}
-                    )"""
-        print(part_query)
-        build_query = f"""insert into "build_information" """\
-                f"""(
-                        build_id,
                         material_type,
                         hatch_distance,
                         num_of_layers,
+                        scan_speed, 
                         layer_thickness,
                         build_time,
+                        number_of_parts,
+                        part_volume,
+                        support_volume,
+                        surface_area,
+                        box_volume,
                         max_build_height,
                         wire_cut,
                         heat_treat,
-                        build_cost
-                    )""" \
-                f"""values ({build_id},
+                        total_cost
+                    ))""" \
+                f"""values ('{customer}',
                             '{material_type}',
                             {hatch_distance},
                             {num_of_layers},
+                            {scan_speed}
                             {layer_thickness},
                             {build_time},
+                            {number_of_parts},
+                            {part_volume},
+                            {support_volume}, 
+                            {surface_area}, 
+                            {box_volume}, 
                             {max_build_height},
                             {wire_cut},
-                            {heat_treat},
-                            {build_cost}
+                            {heat_treat}, 
+                            {total_cost}
                     )"""
-        total_cost = build_cost + mat_cost
-        part_cost = total_cost
-        print(build_query)
-        cost_query = f"""insert into "cost_information" """\
-                f"""(
-                        part_id,
-                        part_cost,
-                        total_cost   
-                    )""" \
-                f"""values (
-                        {part_id},
-                        {part_cost},
-                        {total_cost} 
-                    )"""
-        print(cost_query)
-        cur.execute(part_query)
-        # self.conn.commit()
-        # self.conn.close()
-        # cur = self.conn.cursor()
-        cur.execute(build_query)
-        # cur.commit()
-        # cur.close()
-        # cur = self.conn.cursor()
+        
+        result = self.conn.execute(query)
 
-        cur.execute(cost_query)
-        self.conn.commit()
-        cur.close()
+        return result
 
-        return total_cost, part_cost
-    
 class CostEstimationService:
     def __init__(self):
-        self.model = DataInjection()
+        self.model = PartInfoModel()
         
-    def create(self, 
-    params):
-        return self.model.insert(params['customer'], 
-        params['material_type'], 
-        params['hatch_distance'], 
-        params['num_of_layers'], 
-        params['scan_speed'], 
-        params['layer_thickness'], 
-        params['build_time'], 
-        params['number_of_parts'],
-        params['part_volume'],
-        params['support_volume'],
-        params['surface_area'], 
-        params['box_volume'], 
-        params['max_build_height'], 
-        params['wire_cut'], 
-        params['heat_treat'], 
-        params['build_cost'], 
-        params['mat_cost'], 
-        params['build_id'])
+    def create(self, params):
+        return self.model.create(params["customer"], params["Description"])
